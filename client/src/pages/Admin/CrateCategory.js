c719e028209007c00c333310aaa9ce83cd0a7da8
SaBiT495 <shahedbillah495@gmail.com>
2023-11-01 18:06:47 +0600

    products api crud image done

--- client/src/pages/Admin/CrateCategory.js
+++ client/src/pages/Admin/CrateCategory.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
+import CategoryForm from "../../components/Form/CategoryForm";
+import { Modal } from "antd";
 
 const CrateCategory = () => {
+  const [categories, setCategories] = useState([]);
+  const [name, setName] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post("/api/v1/category/create-category", {
+        name,
+      });
+      if (data?.success) {
+        toast.success(`${name} is created`);
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("something went wrong in input form");
+    }
+  };
+
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data.success) {
+        setCategories(data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("something went wrong in getting category");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
   return (
     <Layout title={"Dashboard - Category Create"}>
       <div className="container-fluid m-3 p-3">
@@ -12,6 +54,49 @@ const CrateCategory = () => {
           </div>
           <div className="col-md-3">
             <h1>Create Category</h1>
+            <div className="p-3 w-50">
+              <CategoryForm
+                handleSubmit={handleSubmit}
+                value={name}
+                setValue={setName}
+              />
+            </div>
+            <div>
+              <table className="table">
+                <thead>
+                  <tr>
+                    {categories?.map((c) => (
+                      <>
+                        <tr>
+                          <td key={c._id}>{c.name}</td>
+                          <td>
+                            <button
+                              className="btn btn-primary ms-2"
+                              onClick={() => setVisible(true)}
+                            >
+                              Edit
+                            </button>
+                            <button className="btn btn-danger ms-2">
+                              Delete
+                            </button>
+                          </td>
+                        </tr>
+                      </>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr></tr>
+                </tbody>
+              </table>
+            </div>
+            <Modal
+              onCancel={() => setVisible(false)}
+              footer={null}
+              open={visible}
+            >
+              <CategoryForm />
+            </Modal>
           </div>
         </div>
       </div>
